c1198ebc2769a0fdb59dc5cfdb9a23de4e034ba6
Krystof Hes <krystof.hes@seznam.cz>
2024-01-14 22:48:08 +0100

    problems with strings

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    string name = "Krys";
+    cout << "Hello world!" << endl;
+    cout << "What is your name?" << endl;
+    // cin >> name;
+    cout << "Hello " << name << endl;
     return 0;
 }
