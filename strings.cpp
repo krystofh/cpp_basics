94478ea7d66eb7d910aa114ccd90ec6187a55188
Krystof Hes <krystof.hes@seznam.cz>
2024-01-14 22:49:59 +0100

    changes

---
+++ strings.cpp
@@ -0,0 +1,16 @@
+// C++ Program to demonstrate use of string keyword
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+	string s = "GeeksforGeeks";
+	string str("GeeksforGeeks");
+
+	cout << "s = " << s << endl;
+	cout << "str = " << str << endl;
+
+	return 0;
+}
